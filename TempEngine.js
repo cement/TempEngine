65281fc3e6dbbefb0c757d3728ac8c9f02bbef9c
cement <32463636@qq.com>
2017-10-23 15:26:53 +0800

    Make it simpler

--- TempEngine.js
+++ TempEngine.js
@@ -1,33 +1,10 @@
-// class TempEngin {
-//     // body...
-//     render(h, d) {
-
-//         var temp = h;
-//         var ref = d.ref || 'data';
-//         var code = 'let ' + ref + '= arguments[0];return `' + temp + '`';
-//         var dcq = Function(`${code}`);
-
-//         return dcq(d);
-//     }
-// }
-// var TempEngin = {}
-// TempEngin.render = function(h, d) {
-//     var temp = h;
-//     var ref = d.ref || 'data';
-//     var code = 'let ' + ref + '= arguments[0];return `' + temp + '`';
-//     var dcq = Function(`${code}`);
-
-//     return dcq(d);
-// }
-// 
-var TempEngine = {
-    render: function(h,d) {
-        var temp = h;
-        var ref = d.name||'data';
-        var code = 'let ' + ref + '= arguments[0];return `' + temp + '`';
-        var run = Function(`${code}`);
+class TempEngine {
+    displace(h, d, r) {
+        let temp = h;
+        let ref = r || d.name || 'data';
+        let code = 'return `' + temp + '`';
+        let run = Function(ref, `${code}`);
         return run(d);
     }
 }
-
 module.exports = TempEngine;
--- test.js
+++ test.js
@@ -1,31 +1,11 @@
+var Engine = require('./TempEngine');
+var engine = new Engine();
+var fs = require('fs');
 
- let text = '\t<!DOCTYPE html>\n'+
-'\t<html lang="en">\n'+
-'\t<head>\n'+
-	'\t<meta charset="UTF-8">\n'+
-	'\t<title>Document</title>\n'+
-'\t</head>\n'+
-'\t<body>\n'+
-    '\t<div>${data.title}</div>\n'+
-	'\t<ul>\n'+
-		'\t<li class="li-1">${data.item[0]}</li>\n'+
-		'\t<li class="li-1">${data.item[1]}</li>\n'+
-		'\t<li class="li-2">${data.item[2]}</li>\n'+
-		'\t<li class="li-3">${data.item[3]}</li>\n'+
-		'\t<li class="li-4">${data.item[4]}</li>\n'+
-		'\t<li class="li-5">${data.item[5]}</li>\n'+
-		'\t<li class="li-6">${data.item[6]}</li>\n'+
-		'\t<li class="li-7">${data.item[7]}</li>\n'+
-		'\t<li class="li-8">${data.item[8]}</li>\n'+
-	'\t</ul>\n'+
-	
-'\t</body>\n'+
-'\t</html>';
-  
- var data = {name:'data',title:'this is aaaaa title',item:['1','2','3','4','5','6','7','8','9']};
-let Engine = require('./TempEngine');
-let res = Engine.render(text,data);
+var r = fs.readFileSync('./HtmlTemp.html',{encoding:'utf8',flag:'r'});
+console.log(r);
+let data = {title:'The displaced title',item:['1','2','3','4','5','6','7','8','9']};
+
+let res = engine.displace(r,data,'d');
 console.log(res);
 
-var a={}
-console.log(a.name)
